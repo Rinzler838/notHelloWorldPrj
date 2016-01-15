1a646f5584ceb6c83d3075c1e3cccba40bc0cf97
Rinzler838 <dvil4688@740s-pl-105.local>
2016-01-15 08:38:24 -0700

    First C++ prj in Github

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,21 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: dvil4688
+ */
+#include <iostream> //Using input and output streams.
+using namespace std; //Access to the Standard library.
+
+int main()
+{
+	cout << "This is how you type to the console in C++" << endl;
+	//C++ equivalent of System.out.println("");
+	int temp;
+
+	cin >> temp;
+
+	cout << temp;
+
+	return 0;
+}
